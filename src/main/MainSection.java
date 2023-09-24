ac2e4af5abfef58087f735fd24045f33da1afd57
YinYinWin <yinyinwin.mdy@gmail.com>
2023-09-24 16:22:31 +0630

    added gitIgnore file and modified MainSection.java

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -5,6 +5,11 @@ public class MainSection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git testing...");
+		System.out.println("This is  modified");
+System.out.println("This is  modified");
+		
 	}
 
 }
+
+
